7e25926ceb4a9eab92f0635da10d8ca000a99f48
Ishmamur Rahman <trnwrckd@gmail.com>
2021-11-30 17:15:30 +0600

    still so much to do

--- src/Components/Pages/Home/About/About.js
+++ src/Components/Pages/Home/About/About.js
@@ -8,11 +8,11 @@ const About = () => {
             <h1 className="section-heading">About</h1>
             <div className="container pt-3 pb-5">
                 <div className="row row-cols-1 row-cols-md-2 g-3">
-                    <div className="bio">
-                        I am fascinated by so many things. Designing, for instance. From simple illustrations to web designing. Some people are gifted but not being one of them, I want to find out how much difference is actually made by putting an effort.
-                        I am keen on Web Development. I've experience with Svelte and React. I have some experience in android development with Flutter.
-                        Being a Machine Learning enthusiast, I spend a fair amount of my time working on my BSc thesis these days. I use Python3.0, scikit-learn and Tensorflow in my codes.
-                        There is so much left to learn, and I have only now discovered that.
+                    <div className="bio px-2">
+                        <p>From websites to simple illustrations, I love designing things from scratch. I spend a significant amount of time visualizing what I'm about to create before jumping in to a project.</p>
+                        <p>Development is sort of an art form to me. I need everything to work perfectly and look beautiful. I believe that a good UX can never be traded off for anything else. </p>
+                        <p>I care about the things i do in order to do them well. </p>
+                        <p>Currently working as a Social Media Designer at Dazzle, but I'm open for Frontend Web Developer roles.</p>
                     </div>
                     <div className="skills">
                         <img className="img-fluid" width="450px" src="https://trnwrckd.github.io/ph-simplePortfolio/images/skillSet.png" alt="" />
--- src/Components/Pages/ProjectDetails/ProjectDetails.js
+++ src/Components/Pages/ProjectDetails/ProjectDetails.js
@@ -25,9 +25,9 @@ const ProjectDetails = () => {
     return (
         <div data-aos="fade-in" className="px-5 pt-3 project-details">
             <div className="d-flex flex-column">
-                <div className="d-flex">
-                    <h1 onClick={()=>{navigate('/')}} className="cursor-pointer"><FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></h1>
-                    <h1 className="ps-4"> {title} </h1>
+                <div className="d-flex align-items-center project-title">
+                    <p onClick={()=>{navigate('/')}} className="cursor-pointer"><FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></p>
+                    <p className="ps-4"> {title} </p>
                 </div>
                 <div className="links">
                     {links.url && <a href={links.url}> Live Site</a>}
@@ -40,28 +40,31 @@ const ProjectDetails = () => {
 
             <div className="px-md-2 px-lg-4 row row-cols-1 row-cols-lg-2 g-3 details-container">
                 <div className="col py-3">
-                    <div className="d-flex flex-column my-auto">
+                    <div className="d-flex flex-column">
                         <div><img src={img} className="img-fluid project-details-img" width="100%" alt="" /></div>
                         <div className="py-3">
-                            <h3 className="text-start">Tech Used</h3>
-                            <div className="row row-cols-4 g-3">
+                            <h3 className="text-start mb-4">Tech Used</h3>
+                            <div className="row row-cols-3 row-cols-md-4 g-3">
                                 {
-                                    techs.map( tech=> <div key={tech} className="p"> {tech}</div> )
+                                    techs.map( tech=> <div key={tech} className="col tech"> {tech}</div> )
                                 }
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col ps-4 py-3">
-                    <h2 className="text-start">Features</h2>
+                <div className="col features text-start ">
+                    <div className="px-0 px-xl-3 py-3">
+
+                    <h1 className="mb-3">Features</h1>
                     {
                         features.map(feature => 
-                            <div key={feature} className = "d-flex align-items-center">
-                                <FontAwesomeIcon icon={faDotCircle}></FontAwesomeIcon>
-                                <p>{feature}</p>
+                            <div key={feature} className= "d-flex justify-content-start">
+                                <small><FontAwesomeIcon icon={faDotCircle}></FontAwesomeIcon></small>
+                                <p className="d-inline-block ps-2">{feature}</p>
                             </div>
                         )
                     }
+                    </div>
                 </div>
             </div>
         </div>
