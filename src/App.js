05c59b4dda84a2ae4e5bc8636ae795edab22a5e4
Ishmamur Rahman <trnwrckd@gmail.com>
2021-12-02 04:17:25 +0600

    scroll to top

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import NotFound from './Components/Pages/NotFound/NotFound';
 import ProjectDetails from './Components/Pages/ProjectDetails/ProjectDetails';
 import Blogs from './Components/Pages/Blogs/Blogs';
 import SlideShow from './Components/Pages/SlideShow/SlideShow';
+import ScrollToTop from './Components/Shared/ScrollToTop/ScrollToTop';
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path = '/' element={<Home/>}/>
-          <Route path = 'home' element={<Home/>}/>
-          <Route path = 'blogs' element={<Blogs/>}/>
-          <Route path = 'slideshow' element={<SlideShow/>}/>
-          <Route path='*' element={<NotFound />} />
-          <Route path = ':id' element={<ProjectDetails/>}></Route>
-        </Routes>
+        <ScrollToTop>
+            <Routes>
+            <Route path = '/' element={<Home/>}/>
+            <Route path = 'home' element={<Home/>}/>
+            <Route path = 'blogs' element={<Blogs/>}/>
+            <Route path = 'slideshow' element={<SlideShow/>}/>
+            <Route path='*' element={<NotFound />} />
+            <Route path = ':id' element={<ProjectDetails/>}></Route>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
